7f4b581a1997b79c099ced3a2dd329bfbdc656f5
Lohelani <lohelani.hicks@gmail.com>
2020-10-28 21:07:19 -0400

    covid api addition

--- assets/js/ticketScript.js
+++ assets/js/ticketScript.js
@@ -3,30 +3,47 @@ $(document).ready(function () {
     var quoteList = []
     var carrierList = []
     var numQuotes = parseInt(localStorage.getItem("numQuotes"))
-    for(x = 1;x<=numQuotes;x++)
-    {
+    for (x = 1; x <= numQuotes; x++) {
         quoteList.push(JSON.parse(localStorage.getItem(x)))
     }
-    for(y = numQuotes;y<localStorage.length;y++)
-    {
+    for (y = numQuotes; y < localStorage.length; y++) {
         count = 0
         carrierList.push(JSON.parse(localStorage.getItem("Carrier" + count)))
-        count ++
+        count++
     }
     console.log(quoteList[0].MinPrice)
     console.log(carrierList[0])
     //console.log(quoteList)
 
     function displayPrices() {
-        for(i=0;i<5;i++)
-        {
-        $("#"+i).text(quoteList[i].MinPrice);
-        $("#"+i+"a").text(carrierList[i].Name);
+        for (i = 0; i < 5; i++) {
+            $("#" + i).text(quoteList[i].MinPrice);
+            //$("#" + i + "a").text(carrierList[i].Name);
         }
     }
     displayPrices();
 
+
+
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://covid-19-data.p.rapidapi.com/help/countries?format=json",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+            "x-rapidapi-key": "1f276bab2bmsh9755c7e198e2120p10a455jsnb0359ae2a1b6"
+        }
+    }
     
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        console.log("working");
+
+    });
+    
+
     //console.log(quoteList.toString())
 
     // var settings = {
@@ -40,15 +57,15 @@ $(document).ready(function () {
     //     }
     //};
 
-   /*  var contentUl = document.createElement("ul")
-    for (var i = 0; i < quoteList.Quotes.length; i++) {
-        console.log("Does this work:" + quoteList.Quotes.length)
-
-        var contentLi = document.createElement("li");
-        contentLi.textContent = "Airline: " + high_score[i].name + " Price: " + high_score[i].score;
-        contentUl.append(contentLi);
-
-    }
-
-    document.body.append(contentUl); */
+    /*  var contentUl = document.createElement("ul")
+     for (var i = 0; i < quoteList.Quotes.length; i++) {
+         console.log("Does this work:" + quoteList.Quotes.length)
+ 
+         var contentLi = document.createElement("li");
+         contentLi.textContent = "Airline: " + high_score[i].name + " Price: " + high_score[i].score;
+         contentUl.append(contentLi);
+ 
+     }
+ 
+     document.body.append(contentUl); */
 });
