911d3c2082ba7d65d5feac9f4bf33555dbb7ed94
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-31 00:27:31 -0400

    project is working

--- assets/js/ticketScript.js
+++ assets/js/ticketScript.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    // var country = pOne
+    // var confirmed = pTwo
+    // var recovered = pThree
+    // var update = pFour
     var quoteList = []
     var carrierList = []
 
@@ -6,15 +10,14 @@ $(document).ready(function () {
     function displayPrices() {
         var quotes = JSON.parse(localStorage.getItem("quotes"));
         var carriers = JSON.parse(localStorage.getItem("carriers"));
-        for (i = 0; i<5; i++){
+        for (i = 0; i < 5; i++) {
             $("#" + i).text("$" + JSON.parse(quotes[count]).MinPrice);
-            
-            for (j = 0; j<carriers.length; j++) {
 
-                if(JSON.parse(quotes[count]).OutboundLeg.CarrierIds[0] == carriers[j].CarrierId)
-                {
+            for (j = 0; j < carriers.length; j++) {
+
+                if (JSON.parse(quotes[count]).OutboundLeg.CarrierIds[0] == carriers[j].CarrierId) {
                     //console.log(carrierList[p].Name + "matches")
-                    var cList = $("<h4>"+carriers[j].Name + "</h4>")
+                    var cList = $("<h4>" + carriers[j].Name + "</h4>")
                     $("#" + i + "a").html(cList);
                 }
 
@@ -22,53 +25,83 @@ $(document).ready(function () {
             count++;
 
         }
-        
+
     }
-//when user clicks more options button, then the next 5 options will show.
-    $(".moreOptions").on("click", function(e) {
+    //when user clicks more options button, then the next 5 options will show.
+    $(".moreOptions").on("click", function (e) {
         //e.prevent.default();
         displayPrices();
         console.log('clicked' + count)
 
     })
-    
+
     displayPrices();
+    var country = localStorage.getItem("country")
+    var covsettings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total?country=" + country,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+            "x-rapidapi-key": "273787c05bmshf3984eb1254cd02p1a20a5jsne40328fc0685"
+        }
+    }
 
+    $.ajax(covsettings).done(function (response) {
+        console.log(response);
 
-function displayCovid() {
-    var country = JSON.parse(localStorage.getItem("country"));
-    var confirmed = JSON.parse(localStorage.getItem("confirmed"));
-    //var recovered = JSON.parse(localStorage.getItem("recovered"));
-    var update = JSON.parse(localStorage.getItem("update"));
+        country = response.data.location
+        confirmed = JSON.stringify(response.data.confirmed)
+        recovered = JSON.stringify(response.data.recovered)
+        update = JSON.stringify(response.data.lastChecked)
+        console.log(JSON.stringify(country))
+        console.log(JSON.stringify(confirmed))
+        console.log(JSON.stringify(recovered))
+        console.log(JSON.stringify(update))
 
-
-    var covidDiv = ("<div class= 'covid'>");
+        console.log("working")
+        var covidDiv = $("<div></div>");
 
         var pOne = $("<p>").text("Country: " + country);
-        //covidDiv.append(pOne);
-        console.log(pOne);
+        covidDiv.append(pOne);
 
         var pTwo = $("<p>").text("Confirmed Cases: " + confirmed);
-        //covidDiv.append(pTwo);
-        console.log(pTwo);
+        covidDiv.append(pTwo);
+
+        var pThree = $("<p>").text("Recovered Cases: " + recovered);
+        covidDiv.append(pThree);
+
+        // var pFour = $("<p>").text("Last Updated: " + update);
+        // covidDiv.append(pFour);
+        $("#covidcontent").append(covidDiv);
 
 
-       // var pThree = $("<p>").text("Recovered Cases: " + recovered);
-        //covidDiv.append(pThree);
+        // localStorage.setItem("country", JSON.stringify(response.country));
+        // localStorage.setItem("confirmed", JSON.stringify(response.confirmed));
+        // localStorage.setItem("recovered", JSON.stringify(response.recovered));
+        // localStorage.setItem("update", JSON.stringify(response.lastUpdate));
+        // window.location.href = "tickets.html"
 
-        var pFour = $("<p>").text("Last Updated: " + update);
-        //covidDiv.append(pFour);
-        console.log(pFour);
+    });
+
+    function displayCovid() {
+        //     var country = JSON.parse(localStorage.getItem("country"));
+        //     var confirmed = JSON.parse(localStorage.getItem("confirmed"));
+        //     var recovered = JSON.parse(localStorage.getItem("recovered"));
+        //     var update = JSON.parse(localStorage.getItem("update"));
+
+        
+
+        
 
 
 
+        
+    }
 
-    $("#covidcontent").append(covidDiv);
 
-    
-
-}
-displayCovid();
+    displayCovid();
 
 
 });
